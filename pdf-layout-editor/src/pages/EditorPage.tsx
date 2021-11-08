5b9a35945bd0b5e1db89146a6c9a867ea9af6a00
Alessandro Soro <Aleeee99@users.noreply.github.com>
2021-11-08 10:44:40 +0100

    #14 Allignment for Items

--- pdf-layout-editor/src/pages/EditorPage.tsx
+++ pdf-layout-editor/src/pages/EditorPage.tsx
@@ -6,18 +6,20 @@ import { Editor, Frame, Element } from '@craftjs/core';
 import { Text } from '../components/draggables/Text';
 import { Settings } from '../components/Settings';
 
+
 const useStyles = makeStyles(() => ({
   root: {
     padding: 0,
     background: 'rgb(252, 253, 253)',
   },
+  
 }));
 
 export default function EditorPage() {
   const classes = useStyles();
 
   return (
-    <div style={{margin: "0 auto", width: "800px"}}>
+    <div style={{width: '1200px', margin:'auto',}}>
       <Typography variant="h5" align="center">PDF-LAYOUT-EDITOR</Typography>
       <Editor
         resolver={{
@@ -26,30 +28,30 @@ export default function EditorPage() {
         }}
       >
         <Topbar />
-        <Grid container spacing={5} style={{ paddingTop: '10px' }}>
-          <Grid item xs>
-            <Frame>
-              <Element
-                canvas
-                is={Container}
-                padding={5}
-                background="#FFF"
-                display="block"
-                width={970}
-                height={300}
-                data-cy="root-container"
-              >
-                <Text fontSize={20} textAlign="left" text="Delete me and start your layout!" data-cy="frame-text" />
-              </Element>
-            </Frame>
+          <Grid container spacing={3} style={{ paddingTop: '10px' }}>
+            <Grid item xs>
+              <Frame>
+                <Element
+                  canvas
+                  is={Container}
+                  padding={5}
+                  background="#FFF"
+                  display="block"
+                  width={800}
+                  height={300}
+                  data-cy="root-container"
+                >
+                  <Text fontSize={20} textAlign="left" text="Delete me and start your layout!" data-cy="frame-text" />
+                </Element>
+              </Frame>
+            </Grid>
+            <Grid item xs={3}>
+              <Paper className={classes.root}>
+                <Toolbox />
+                <Settings />
+              </Paper>
+            </Grid>
           </Grid>
-          <Grid item xs={3}>
-            <Paper className={classes.root}>
-              <Toolbox />
-              <Settings />
-            </Paper>
-          </Grid>
-        </Grid>
       </Editor>
     </div>
   );
